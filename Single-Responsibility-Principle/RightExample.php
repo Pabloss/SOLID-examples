f5cc7d802443e5947fb68db6cb6a4e7491f2b3eb
Paweł Zimnowodzki <pawel.zimnowodzki@gmail.com>
2017-11-22 14:51:08 +0100

    explaining

--- Single-Responsibility-Principle/RightExample.php
+++ Single-Responsibility-Principle/RightExample.php
@@ -1,6 +1,16 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * RightExample and Render classes are "right" cases of SRP
+ * because of obvious separation:
+ * 1. RightExample only gives data
+ * 2. Render only takes data and wraps it by html tags
+ *
+ * There will be less possibility that one these two classes would be changed when requirements were changed too.
+ * All required changes for html shape will focus only on Render class.
+ */
+
 class RightExample
 {
     private $data = [];
@@ -11,11 +21,12 @@ class RightExample
     }
 }
 
-class Render{
+class Render
+{
     public function toHtml(array $data)
     {
         $result = "<ul>\n";
-        foreach($data as $key => $item){
+        foreach ($data as $key => $item) {
             $result .= "<li><a href='$key'>$item</a></li>\n";
         }
         $result .= "</ul>\n";
--- Single-Responsibility-Principle/WrongExample.php
+++ Single-Responsibility-Principle/WrongExample.php
@@ -1,7 +1,18 @@
 <?php
 declare(strict_types=1);
 
-
+/**
+ * WrongExample class is "wrong" case of SRP
+ * because:
+ *
+ * When we changes e.g. source of data we should change WrongExample class.
+ * And when we're changing html structure we should change WrongExample class too.
+ *
+ * Then in extreme case when whole application contains classes with maximum levels of responsibility,
+ * all even smallest change would (in high chance) propagate all over the application. And that case
+ * is hardest to maintain when we "afraid" to touch the code because that "chain reaction effect"
+ *
+ */
 class WrongExample
 {
     private $data = [];
@@ -14,7 +25,7 @@ class WrongExample
     public function renderHtml(array $data): string
     {
         $result = "<ul>\n";
-        foreach($data as $key => $item){
+        foreach ($data as $key => $item) {
             $result .= "<li><a href='$key'>$item</a></li>\n";
         }
         $result .= "</ul>\n";
