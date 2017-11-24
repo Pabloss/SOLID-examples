009cae9c2f1f0e52d3a8fd5c3b3cbcc719172ba3
Paweł Zimnowodzki <pawel.zimnowodzki@gmail.com>
2017-11-24 15:33:11 +0100

    completing of LSP

--- test/SOLIDExamples/LiskovSubistutionPrinciple/RectangleTest.php
+++ test/SOLIDExamples/LiskovSubistutionPrinciple/RectangleTest.php
@@ -7,7 +7,6 @@ use PHPUnit\Framework\TestCase;
 use SOLIDExamples\LiskovSubstitutionPrinciple\Rectangle;
 use SOLIDExamples\LiskovSubstitutionPrinciple\Square;
 
-
 /**
  * Liskov Substitution Principle is the stronger than rule than only simple inheritance.
  * It requests to copy BEHAVIOR of base classes to inherited from them. Inheritance should be somehow "transparent" i.e.
@@ -17,13 +16,27 @@ use SOLIDExamples\LiskovSubstitutionPrinciple\Square;
 class RectangleTest extends TestCase
 {
     /**
+     * It would be an example of conditions that DOES NOT meet LSP principle
+     *
      * @test
      * @dataProvider additionProvider
      */
     public function draw($expected, Rectangle $rectangle)
     {
         /** Some kind of draw process */
-        self::assertEquals($expected, $rectangle->getX() + $rectangle->getWidth());
+        self::assertEquals($expected, $rectangle->getX() + $rectangle->getWidth()); // <= some data from additionProvider could FAIL the test
+    }
+
+    /**
+     * It would be an example of conditions that meet LSP principle
+     *
+     * @test
+     * @dataProvider areaProvider
+     */
+    public function area($expected, Rectangle $rectangle)
+    {
+        /** Some kind of draw process */
+        self::assertEquals($expected, $rectangle->getWidth() * $rectangle->getHeight()); // <= all data from  areaProvider will PASS the test
     }
 
     public function additionProvider()
@@ -36,6 +49,19 @@ class RectangleTest extends TestCase
                 [12, Rectangle::getInstance(0, 1, 12, 17)],
                 [15, Square::getInstance(0, 1, 15, 12)], // <= that test FAILS because we could don't now that width is replaced by height in squarea
                 [12, Rectangle::getInstance(0, 1, 12, 17)],
+        ];
+    }
+
+    public function areaProvider()
+    {
+        return [
+            [12*17, Rectangle::getInstance(0, 1, 12, 17)],
+            [9*12, Rectangle::getInstance(0, 1, 9, 12)],
+            [2*8, Rectangle::getInstance(0, 1, 2, 8)],
+            [17*17, Square::getInstance(0, 1, 12, 17)], // <= that test FAILS because we could don't now that width is replaced by height in square
+            [12*17, Rectangle::getInstance(0, 1, 12, 17)],
+            [12*12, Square::getInstance(0, 1, 15, 12)], // <= that test FAILS because we could don't now that width is replaced by height in squarea
+            [12*17, Rectangle::getInstance(0, 1, 12, 17)],
         ];
     }
 }
